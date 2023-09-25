50f948c866d913543e5126c832ada739f0698de2
Omnia Alwazzan <37316580+omniaalwazzan@users.noreply.github.com>
2023-09-25 23:02:56 +0100

    Create save_bVal_mVal.r

---
+++ save_bVal_mVal.r
@@ -0,0 +1,39 @@
+# NOTE: in Champ we need to place the csv sample_sheet inside the folder containing idat files 
+
+library("ChAMP")
+## we should always include \\ in any path or we get an error
+
+dataDirectory <- "D:\\2ndYear\\temp\\Methylation_data_all_tumours_May23\\Part_1_001_050\\#002_450k Array Data 2015-02\\9989536145\\"
+
+myLoad <- champ.load(dataDirectory,arraytype="450K")
+myImport <- champ.import(dataDirectory)
+
+# save beta
+csv_path_beta <- "D:\\2ndYear\\Slivia's project\\mythalExp\\batch1\\betaV.csv"
+write.csv(myImport$beta, csv_path, row.names=T)
+
+# Save m-Values
+csv_path_mVal <- "D:\\2ndYear\\Slivia's project\\mythalExp\\batch1\\mVal.csv"
+write.csv(myImport$M, csv_path, row.names=T)
+
+####### This block we need to do it after extracting all beta & M vlaues from all samples ######
+# myfilter <- champ.filter(beta=myImport$beta,M=myImport$M,pd=myImport$pd,detP=myImport$detP,beadcount=myImport$beadcount)
+# CpG.GUI(CpG=rownames(myfilter$M),arraytype="450K")
+# par("mar")
+# par(mar=c(1,1,1,1))
+# champ.QC()
+# QC.GUI(beta=myfilter$beta,arraytype="450K")
+# myNormBeta <- champ.norm(beta=myfilter$beta,arraytype="450K",cores=5)
+#champ.SVD(beta=myNormBeta %>% as.data.frame(), pd=myLoad$pd)
+
+#############
+# Normalize m vlaues is not working an the followin is the error
+#Error in champ.BMIQ(beta[, x], design.v, sampleID = colnames(beta)[x],  : 
+#                      task 2 failed - "need at least 2 points to select a bandwidth automatically"
+#myNorm <- champ.norm(beta=myfilter$M,arraytype="450K",cores=5)
+
+
+
+## runcombat is not working on one sample group
+#myCombat <- champ.runCombat(beta=myfilter$M,pd=myLoad$pd,batchname=c("Sample_Name"))
+
